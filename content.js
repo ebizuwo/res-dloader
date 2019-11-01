ec20e1a58de60fe150aea96f42663b56f5d974a9
Ermias Bizuwork <ermiasbizuwork@Ermiass-MacBook-Air.local>
2019-11-01 10:05:07 -0400

    non stable version of master

--- content.js
+++ content.js
@@ -1,1 +1,14 @@
-alert("You're on a page bro")
+alert("content is running")
+let page_div = document.getElementsByClassName("rc-CML styled").childNodes.values();
+document.querySelector("#rendered-content > div > div > div > main > div > div > section > div")
+let a = document.getElementsByTagName("a");
+
+
+let i;
+let page_links = [];
+for ( i=0;i < a.length; i++){
+    page_links.push(a[i])
+}
+alert(page_div)
+// console.log(page_links);
+// alert(page_links.toString())
